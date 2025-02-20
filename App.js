d6652a8f61b465618bcc5d72a0d70f9086937389
TheTanukiMDK <riquicraft04@gmail.com>
2025-02-20 09:05:02 -0500

    Coexion a api con FlatList

--- App.js
+++ App.js
@@ -1,27 +1,35 @@
 import { StatusBar } from 'expo-status-bar';
-import { FlatList, StyleSheet, Text, TextInput, View } from 'react-native';
-import { SafeAreaProvider,SafeAreaView } from 'react-native-safe-area-context';
-//import Cat from './components/Cat'
+import { FlatList, StyleSheet, Text, TextInput, View, Image } from 'react-native';
+import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
 import { useEffect, useState } from 'react';
+
 export default function App() {
-  const [data,setData] = useState([]);
+  const [data, setData] = useState([]);
 
   useEffect(() => {
-    fetch("https://jsonplaceholder.typicode.com/posts")
-    .then((response) => response.json())
-    .then((json) => setData(json))
-    .catch((error) => console.error
-    (error
-    ));
+    fetch("https://rickandmortyapi.com/api/character")
+      .then((response) => response.json())
+      .then((json) => setData(json.results))
+      .catch((error) => console.error(error));
   }, []);
+
+  const renderItem = ({ item }) => (
+    <View style={styles.item}>
+      <Image source={{uri: item.image}} style={styles.image}></Image>
+      <Text style={styles.name}>{item.name}</Text>
+      <Text>{item.status}</Text>
+    </View>
+  );
+
   return (
     <>
       <SafeAreaProvider>
-        <SafeAreaView>
-          <FlatList data={data}
-          keyExtractor={item => item.id.toString()}
-          >
-          </FlatList>
+        <SafeAreaView style={styles.container}>
+          <FlatList
+            data={data}
+            keyExtractor={item => item.id.toString()}
+            renderItem={renderItem}
+          />
         </SafeAreaView>
       </SafeAreaProvider>
     </>
@@ -34,5 +42,21 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  item: {
+    backgroundColor: '#f9c2ff',
+    padding: 20,
+    marginVertical: 8,
+    marginHorizontal: 16,
+    alignItems: 'center',
+  },
+  name: {
+    fontSize: 24,
+    fontWeight: 'bold',
+  },
+  image: {
+    width: 100,
+    height: 100,
+    borderRadius: 50,
   },
 });
